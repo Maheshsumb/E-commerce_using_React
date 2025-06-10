cfb7cddabb300b5c9c3c9f42ee7c8850c58b7816
MaheshSumb <maheshsumbpatil87@gmail.com>
2025-06-10 19:51:39 +0530

    Sidebar complted

--- admin/src/components/Sidebar/index.js
+++ admin/src/components/Sidebar/index.js
@@ -9,6 +9,7 @@ import { IoMdSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
+import { IoMdLogOut } from "react-icons/io";
 
 const Sidebar = () => {
   const [isActiveButton, setisActiveButton] = useState(0);
@@ -132,185 +133,15 @@ const Sidebar = () => {
               </Button>
             </Link>
           </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 6 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <RiDashboardFill />
-                </span>
-                Dashboard
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 7 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <MdGames />
-                </span>
-                Products
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 8 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdCart />
-                </span>
-                Orders
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 9 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <MdMessage />
-                </span>
-                Messages
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 10 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdNotifications />
-                </span>
-                Notifications
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 11 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdSettings />
-                </span>
-                Settings
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 12 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <RiDashboardFill />
-                </span>
-                Dashboard
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Button
-              className={`w-100 ${isActiveButton === 13 ? "active" : ""}`}
-            >
-              <span className="icon">
-                <MdGames />
-              </span>
-              Products
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 14 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdCart />
-                </span>
-                Orders
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 15 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <MdMessage />
-                </span>
-                Messages
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 16 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdNotifications />
-                </span>
-                Notifications
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Button
-                className={`w-100 ${isActiveButton === 17 ? "active" : ""}`}
-              >
-                <span className="icon">
-                  <IoMdSettings />
-                </span>
-                Settings
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
-          </li>
         </ul>
+        <br />
+        <div className="logoutWrapper">
+          <div className="logoutBox">
+            <Button variant="contained">
+              <IoMdLogOut /> Logout
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
